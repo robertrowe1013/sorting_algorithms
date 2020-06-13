e3162ba9ce90b9c5dfa08e41e6c86eaaa009317d
robertrowe1013 <robertrowe1013@gmail.com>
2020-06-13 15:39:08 +0000

    shell sort task

--- sort.h
+++ sort.h
@@ -33,4 +33,11 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
 void cocktail_sort_list(listint_t **list);
+
+int split(int *arr, int left, int right, size_t size);
+
+void sort_alg(int *arr, int left, int right, size_t size);
+
+void shell_sort(int *array, size_t size);
+
 #endif
