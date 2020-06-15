98b7f3fd4495e0740a806211bb5252d497483ffc
Brett Davis <brett.p.davis101@gmail.com>
2020-06-15 02:28:51 +0000

    Changes to task 0 that might fix that req check Adds return to documentation - shouldnt be needed, but worth a shot Adds NULL check and check for size less than 2 - Should be an ouput check if anything, but not impossible

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,12 +5,16 @@
  * using the Bubble sort algorithm
  * @array: Array to sort
  * @size: size of the array
+ *
+ * Return: Void
  */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, size2 = size;
 	int tmp, flag = 0;
 
+	if (array == NULL || size < 2)
+		return;
 	for (i = 0; i < size; i++)
 	{
 		for (j = 1; j < size2; j++)
